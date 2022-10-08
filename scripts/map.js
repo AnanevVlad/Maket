9edb68fac5279da72c65af742ca7dea7013b60a6
Vlad <ananevvlad12@gmail.com>
2022-10-08 16:45:45 +0300

    map

--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,15 @@
 
         function initMap(){
+          var pos={lat: 21.957350138701074,lng: -159.35599926326904}
             var opt={
-                center:{lat: 21.95735717483223,lng: -159.35599641835665},
+                center:pos,
                 zoom:20
+             
             }
             var map=new google.maps.Map(document.getElementById("map"),opt);
+            var marker= new google.maps.Marker({
+              position:pos,
+              map:map
+            })
             
         };
