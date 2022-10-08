8ee7954322179b7843de45b72664d8925a0bf1b2
Vlad <ananevvlad12@gmail.com>
2022-10-08 16:30:21 +0300

    map

---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+
+        function initMap(){
+            var opt={
+                center:{lat: 21.95735717483223,lng: -159.35599641835665},
+                zoom:20
+            }
+            var map=new google.maps.Map(document.getElementById("map"),opt);
+            
+        };
